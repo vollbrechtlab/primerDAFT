6d78af535d39f69b617135c349967ce5697dfaeb
Takao <tshibamot42@students.tntech.edu>
2018-01-16 16:19:50 -0600

    add spec check

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
-setup(name='primer-blast-dx',
+setup(name='primer blast dx',
       version='0.1',
       description='simple primer blast',
       url='https://github.com/takao42/primer-blast-dx',
       author='Takao',
       author_email='takaos@iastate.edu',
       license='MIT',
-      packages=['primer-blast-dx'],
+      packages=['primer_blast_dx'],
+      install_requires=[
+          'primer3-py',
+          'biopython',
+          'pandas'
+      ],
       zip_safe=False)
---
+++ test/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env
+
+import primer_blast_dx
+
+primer_blast_dx.findPrimersFile("0Y7Cnlt4E37SP2W9_task.json", "someresult.json")
