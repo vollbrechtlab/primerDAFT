16539b63a185ec982e4a5433303e64000a54cf9f
Takao <tshibamot42@students.tntech.edu>
2018-02-09 13:54:26 -0600

    spec check now returns dictionary

--- test/test.py
+++ test/test.py
@@ -4,4 +4,12 @@ import primer_blast_dx
 
 #primer_blast_dx.findPrimersFile("0Y7Cnlt4E37SP2W9_task.json", "someresult.json")
 
-primer_blast_dx.specCheck("0Y7Cnlt4E37SP2W9_task.json", "someresult.json")
+result = primer_blast_dx.specCheck("0Y7Cnlt4E37SP2W9_task.json", "someresult.json")
+print(result)
+
+"""
+out_json=open(tmp_id+".out.json","w")
+#pp.pprint(data)
+json.dump(data,out_json,indent="\t",sort_keys=True)
+out_json.close()
+"""
