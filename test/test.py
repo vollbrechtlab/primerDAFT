cb515a3d42dd06f7f63c3ba598fe3517e49bb5a1
Takao <tshibamot42@students.tntech.edu>
2018-02-09 14:50:43 -0600

    making run function

--- test/test.py
+++ test/test.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env
 
 import primer_blast_dx
+from pprint import pprint
+import json
 
 #primer_blast_dx.findPrimersFile("0Y7Cnlt4E37SP2W9_task.json", "someresult.json")
 
-result = primer_blast_dx.specCheck("0Y7Cnlt4E37SP2W9_task.json", "someresult.json")
-print(result)
+def test_specCheck():
+    result = primer_blast_dx.specCheck("0Y7Cnlt4E37SP2W9_task.json", "someresult.json")
+    print(result)
+
+def test_findPrimers():
+
+    taskFilePath = "0Y7Cnlt4E37SP2W9_task.json"
+    with open(taskFilePath) as taskFile:
+        taskData = json.load(taskFile)
+
+    result = primer_blast_dx.findPrimers(taskData['input_data'])
+    print(result)
+
+def test_run():
+    taskFilePath = "0Y7Cnlt4E37SP2W9_task.json"
+    with open(taskFilePath) as taskFile:
+        taskData = json.load(taskFile)
+
+    result = primer_blast_dx.run(taskData)
+    print(result)
+
+test_run()
+
 
 """
 out_json=open(tmp_id+".out.json","w")
