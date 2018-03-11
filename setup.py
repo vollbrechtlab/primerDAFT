4bb43144e60d466698722c6b4e039015a1cdf2ff
Kokulapalan Wimalanathan <kokul@iastate.edu>
2018-03-11 14:34:50 -0500

    added test script to bin dir

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='primer blast dx',
+setup(name='primer_blast_dx',
       version='0.1',
       description='simple primer blast',
       url='https://github.com/takao42/primer-blast-dx',
