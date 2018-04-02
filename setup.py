6cdbf43d496f535ca1a501487eff981021b48693
Takao Shibamoto <takao8@gmx.com>
2018-04-02 03:36:09 -0500

    trying to fix nonadded error

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='primerDAFT',
-      version='0.3',
+      version='0.2',
       description='A Python based PCR Primer Design and Filtering Package',
       url='https://github.com/vollbrechtlab/primerDAFT',
       author='Takao Shibamoto',
       author_email='takaos@iastate.edu',
       license='MIT',
-      packages=['primerDAFT'],
+      packages=find_packages(exclude=['venv','test','sphinx','docs','diagram']),
       scripts=['bin/asd.py'],
       install_requires=[
           'primer3-py',
