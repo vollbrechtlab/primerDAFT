66e4b905d2292ade791e68f482353b03e3cfbf46
Takao Shibamoto <takao8@gmx.com>
2018-04-02 02:51:57 -0500

    version 0.2

--- venv/bin/asd.py
+++
@@ -1,6 +0,0 @@
-#!/mnt/data/my-repo/primerDAFT/venv/bin/python3
-# EASY-INSTALL-DEV-SCRIPT: 'primerDAFT==0.2','asd.py'
-__requires__ = 'primerDAFT==0.2'
-__import__('pkg_resources').require('primerDAFT==0.2')
-__file__ = '/mnt/data/my-repo/primerDAFT/bin/asd.py'
-exec(compile(open(__file__).read(), __file__, 'exec'))
